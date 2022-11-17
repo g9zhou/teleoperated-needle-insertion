1ffd343ec78491cde05c1048129d881f42abf419
g9zhou <116707042+g9zhou@users.noreply.github.com>
2022-11-17 12:43:44 -0800

    Create needle_insertion.ino

---
+++ needle_insertion/needle_insertion.ino
@@ -0,0 +1,247 @@
+//--------------------------------------------------------------------------
+// Code to test basic Hapkit functionality (sensing and force output)
+// Code updated by Cara Nunez 4.17.19
+//--------------------------------------------------------------------------
+// Parameters that define what environment to render
+#define ENABLE_NEEDLE_INSERTION
+
+// Includes
+#include <math.h>
+#include <AS5048A.h>
+
+// the sensor CSn pin is connected to pin 10
+AS5048A angleSensor(10);
+
+// Pin declares
+int pwmPin = 5; // PWM output pin for motor 1
+int dirPin = 8; // direction output pin for motor 1
+int sensorPosPin = A2; // input pin for MR sensor
+int fsrPin = A3; // input pin for FSR sensor
+
+// Position tracking variables
+long updatedPos = 0;     // keeps track of the latest updated value of the MR sensor reading
+int rawPos = 0;         // current raw reading from MR sensor
+int lastRawPos = 0;     // last raw reading from MR sensor
+int lastLastRawPos = 0; // last last raw reading from MR sensor
+int flipNumber = 0;     // keeps track of the number of flips over the 180deg mark
+int tempOffset = 0;
+int rawDiff = 0;
+int lastRawDiff = 0;
+int rawOffset = 0;
+int lastRawOffset = 0;
+const int flipThresh = 15000;  // threshold to determine whether or not a flip over the 180 degree mark occurred
+boolean flipped = false;
+
+// Kinematics variables
+double xh = 0;           // position of the handle [m]
+double theta_s = 0;      // Angle of the sector pulley in deg
+double xh_prev;          // Distance of the handle at previous time step
+double xh_prev2;
+double dxh;              // Velocity of the handle
+double dxh_prev;
+double dxh_prev2;
+double dxh_filt;         // Filtered velocity of the handle
+double dxh_filt_prev;
+double dxh_filt_prev2;
+
+// Force output variables
+double force = 0;           // force at the handle
+double Tp = 0;              // torque of the motor pulley
+double duty = 0;            // duty cylce (between 0 and 255)
+unsigned int output = 0;    // output command to the motor
+
+
+// --------------------------------------------------------------
+// Setup function -- NO NEED TO EDIT
+// --------------------------------------------------------------
+void setup() 
+{
+  // Set up serial communication
+  Serial.begin(115200);
+  
+  // Set PWM frequency 
+  setPwmFrequency(pwmPin,1); 
+  
+  // Input pins
+  pinMode(sensorPosPin, INPUT); // set MR sensor pin to be an input
+  pinMode(fsrPin, INPUT);       // set FSR sensor pin to be an input
+
+  // Output pins
+  pinMode(pwmPin, OUTPUT);  // PWM pin for motor A
+  pinMode(dirPin, OUTPUT);  // dir pin for motor A
+  
+  // Initialize motor 
+  analogWrite(pwmPin, 0);     // set to not be spinning (0/255)
+  digitalWrite(dirPin, LOW);  // set direction
+
+  angleSensor.init();
+  // Initialize position valiables
+  angleSensor.getRawRotation(); // take reading in case first reading is spurious
+  lastLastRawPos = angleSensor.getRawRotation(); // position from Hall Effect sensor
+  lastRawPos = angleSensor.getRawRotation(); // position from Hall Effect sensor
+}
+
+
+// --------------------------------------------------------------
+// Main Loop
+// --------------------------------------------------------------
+void loop()
+{
+  
+  //*************************************************************
+  //*** Section 1. Compute position in counts (do not change) ***  
+  //*************************************************************
+
+  // Get voltage output by MR sensor
+  rawPos = int(angleSensor.getRawRotation());  //current raw position from MR sensor
+
+  // Calculate differences between subsequent MR sensor readings
+  rawDiff = rawPos - lastRawPos;          //difference btwn current raw position and last raw position
+  lastRawDiff = rawPos - lastLastRawPos;  //difference btwn current raw position and last last raw position
+  rawOffset = abs(rawDiff);
+  lastRawOffset = abs(lastRawDiff);
+  
+  // Update position record-keeping vairables
+  lastLastRawPos = lastRawPos;
+  lastRawPos = rawPos;
+  
+  // Keep track of flips over 180 degrees
+  if((lastRawOffset > flipThresh) && (!flipped)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
+    if(lastRawDiff > 0) {        // check to see which direction the drive wheel was turning
+      flipNumber--;              // cw rotation 
+    } else {                     // if(rawDiff < 0)
+      flipNumber++;              // ccw rotation
+    }
+    if(rawOffset > flipThresh) { // check to see if the data was good and the most current offset is above the threshold
+      updatedPos = rawPos + flipNumber*rawOffset; // update the pos value to account for flips over 180deg using the most current offset 
+      tempOffset = rawOffset;
+    } else {                     // in this case there was a blip in the data and we want to use lastactualOffset instead
+      updatedPos = rawPos + flipNumber*lastRawOffset;  // update the pos value to account for any flips over 180deg using the LAST offset
+      tempOffset = lastRawOffset;
+    }
+    flipped = true;            // set boolean so that the next time through the loop won't trigger a flip
+  } else {                        // anytime no flip has occurred
+    updatedPos = rawPos + flipNumber*tempOffset; // need to update pos based on what most recent offset is 
+    flipped = false;
+  }
+ 
+  //*************************************************************
+  //*** Section 2. Compute position in meters *******************
+  //*************************************************************
+
+  // ADD YOUR CODE HERE (Use your code from Problems 1 and 2)
+  // Define kinematic parameters you may need
+  double rh = 0.09;   //[m]
+  // Step B.1: print updatedPos via serial monitor
+//  Serial.println(updatedPos);
+  // Step B.6: double ts = ?; // Compute the angle of the sector pulley (ts) in degrees based on updatedPos
+  double ts = -0.002017257801552*updatedPos + 20.594368283120627;
+  // Step B.7: xh = ?;       // Compute the position of the handle (in meters) based on ts (in radians)
+  xh = ts/180*M_PI*rh;
+  // Step B.8: print xh via serial monitor
+//  Serial.println(xh);
+
+  // Calculate velocity with loop time estimation
+  dxh = (double)(xh - xh_prev) / 0.001;
+
+  // Calculate the filtered velocity of the handle using an infinite impulse response filter
+  dxh_filt = .9*dxh + 0.1*dxh_prev; 
+    
+  // Record the position and velocity
+  xh_prev2 = xh_prev;
+  xh_prev = xh;
+  
+  dxh_prev2 = dxh_prev;
+  dxh_prev = dxh;
+  
+  dxh_filt_prev2 = dxh_filt_prev;
+  dxh_filt_prev = dxh_filt;
+  
+  //*************************************************************
+  //*** Section 3. Assign a motor output force in Newtons *******  
+  //*************************************************************
+  //*************************************************************
+  //******************* Rendering Algorithms ********************
+  //*************************************************************
+  
+#ifdef ENABLE_NEEDLE_INSERTION
+  if ( xh >= -0.03 && xh < 0.02 ) {
+    force = 160*(xh+0.03)*dxh;
+//    Serial.println("fat");
+  } else if ( xh >= 0.02 && xh < 0.04) {
+    force = 100/1.5*(xh-0.02)+0.8;
+//    Serial.println("mem");
+  } else if ( xh >= 0.04 ) {
+    force = (30*(xh-0.04)+11.2)*dxh;
+//    Serial.println("liver");
+  } else {
+    force = 0;
+  }
+#endif
+
+  // ADD YOUR CODE HERE (Use ypur code from Problems 1 and 2)
+  // Define kinematic parameters you may need
+  double rp = 0.005;   //[m]
+  double rs = 0.074;   //[m] 
+  // Step C.1: force = ?; // You can  generate a force by assigning this to a constant number (in Newtons) or use a haptic rendering / virtual environment
+  // Step C.2: Tp = ?;    // Compute the require motor pulley torque (Tp) to generate that force using kinematics
+  Tp = force*rh/rs*rp;
+  Serial.println(xh,5);
+  
+  //*************************************************************
+  //*** Section 4. Force output (do not change) *****************
+  //*************************************************************
+  
+  // Determine correct direction for motor torque
+  if(force > 0) { 
+    digitalWrite(dirPin, HIGH);
+  } else {
+    digitalWrite(dirPin, LOW);
+  }
+
+  // Compute the duty cycle required to generate Tp (torque at the motor pulley)
+  duty = sqrt(abs(Tp)/0.03);
+
+  // Make sure the duty cycle is between 0 and 100%
+  if (duty > 1) {            
+    duty = 1;
+  } else if (duty < 0) { 
+    duty = 0;
+  }  
+  output = (int)(duty* 255);   // convert duty cycle to output signal
+  analogWrite(pwmPin,output);  // output the signal
+}
+
+// --------------------------------------------------------------
+// Function to set PWM Freq -- DO NOT EDIT
+// --------------------------------------------------------------
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x7; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
